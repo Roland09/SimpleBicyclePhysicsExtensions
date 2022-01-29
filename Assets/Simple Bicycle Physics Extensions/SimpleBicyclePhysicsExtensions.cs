6bf53df1824987071f43be8c32f23be79468e3bf
Roland <10963432+Roland09@users.noreply.github.com>
2022-01-29 17:30:55 +0100

    Initial Commit

---
+++ Assets/Simple Bicycle Physics Extensions/SimpleBicyclePhysicsExtensions.cs
@@ -0,0 +1,56 @@
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Rowlan.SimpleBicyclePhysicsExtensions
+{
+    /// <summary>
+    /// Perform the mapping of a Character Creator 3 Character to the IK Rigs of Simple Bicycle Physics
+    /// 
+    /// Usage:
+    /// 
+    /// + drag the CC3 model on the bike root gameobject
+    /// + select the model in the hierarchy, then click menu Cyclist Setup > Setup Selected
+    /// + set x/z to 0/0 (don't change y)
+    /// + perform mapping
+    /// 
+    /// </summary>
+    public class SimpleBicyclePhysicsExtensions : EditorWindow
+    {
+        [SerializeField]
+        private VisualTreeAsset m_VisualTreeAsset = default;
+
+        [MenuItem("Window/Rowlan/Simple Bicycle Physics Extensions")]
+        public static void ShowExample()
+        {
+            SimpleBicyclePhysicsExtensions wnd = GetWindow<SimpleBicyclePhysicsExtensions>();
+            wnd.titleContent = new GUIContent("Simple Bicycle Physics Extensions");
+        }
+
+        private ObjectField bikePrefabRootField;
+
+        public void CreateGUI()
+        {
+            VisualElement root = rootVisualElement;
+
+            // Instantiate UXML
+            VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
+            root.Add(labelFromUXML);
+
+            Button mapCC3Button = root.Q<Button>("MapCC3Button");
+            mapCC3Button.clicked += MapCharacterCreator3;
+
+            this.bikePrefabRootField = root.Q<ObjectField>("BikePrefabRootField");
+
+        }
+
+        private void MapCharacterCreator3()
+        {
+            GameObject bikePrefabRoot = this.bikePrefabRootField.value as GameObject;
+
+            CharacterCreator3Mapper mapper = new CharacterCreator3Mapper();
+            mapper.MapCharacterCreator3(bikePrefabRoot);
+        }
+    }
+}
